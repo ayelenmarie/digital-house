15a192c3986c8e9850e1016b5cf86bfd86ecf599
Ayelen <ayelen.guini@gmail.com>
2023-04-05 01:37:17 -0300

    Created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Bienvenido de vuelta!</Text>
+      <Text>Ruben Rodriguez</Text>
+    </View>
+  );
+};
+
+export default Header;
